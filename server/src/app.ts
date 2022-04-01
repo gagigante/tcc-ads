960e7ebccff44d0b8d5ead72eb2957edc32d8e16
Gabriel Gigante <gabriel.silva@luizalabs.com>
2022-04-01 20:32:37 -0300

    feat: setup servers structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import { routes } from './routes';
+import { AppError } from './errors/AppError';
 
 export const app = express();
 
-app.get('/', (request: Request, response: Response) => {
-  return response.status(200).json({ hello: 'world' });
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, nextFunction: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
