5181035051e384ab000c422f0c5275aad42f998c
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-05-23 19:24:10 -0300

    feat(web): create card item component

---
+++ web/src/components/CardItem/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+import styles from './styles.module.scss';
+
+type CardItemProps = {
+  title: string;
+  imageUrl: string;
+  redirectPath: string;
+}
+
+export const CardItem = ({ title, imageUrl, redirectPath }: CardItemProps) => {
+  return (
+    <Link href={redirectPath}>
+      <a style={{ textDecoration: 'none' }}>
+        <div className={styles.container}>
+          <img src={imageUrl} alt={title} />
+          <strong>{title}</strong>
+        </div>
+      </a>
+    </Link>
+  );
+}
