76272d7b022722a588cb3b01483a81e0e573bbd2
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-06-07 20:23:22 -0300

    feat(client): creating login page

---
+++ web/src/pages/sign-in.tsx
@@ -0,0 +1,100 @@
+import { useEffect } from 'react'
+import type { NextPage } from 'next'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+
+import { useRouter } from 'next/router'
+
+import { LinkButton } from '../components/LinkButton'
+
+import styles from '../styles/pages/signIn.module.scss'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useAuth } from '../hooks/useAuth'
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object({
+  email: yup.string().required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória'),
+})
+
+const SignIn: NextPage = () => {
+  const { push } = useRouter();
+  const { user, signIn } = useAuth();
+  const { register, handleSubmit, formState } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  useEffect(() => {
+    if (user) push('/');
+  }, [])
+  
+  const handleSignIn: SubmitHandler<SignInFormData> = async ({ email, password }) => {
+    await signIn({
+      email,
+      password,
+    });
+  }
+
+  const { errors } = formState
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(handleSignIn)}>
+        <h1>Fazer login</h1>
+
+        <div className={styles.inputContainer}>
+          <Input             
+            type="email" 
+            label="E-mail" 
+            placeholder="Digite seu e-mail"
+            hasError={!!errors.email}
+            {...register('email')}
+          />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <Input
+            type="password"
+            label="Senha"
+            placeholder="Digite sua senha"
+            hasError={!!errors.password}
+            {...register('password')}
+          />
+        </div>
+
+        <div className={styles.linkButtonContainer}>
+          <LinkButton 
+            text="Esqueci minha senha" 
+            type="button" 
+            onClick={() => push('/forgot-password')}
+          />
+        </div>
+
+        <Button 
+          text="Entrar"
+          type="submit"
+          variant="primary"
+          fullWidth 
+          isLoading={formState.isSubmitting} 
+        />
+
+        <div className={styles.createAccountButtonContainer}>
+          <LinkButton 
+            text="Criar conta"
+            type="button" 
+            onClick={() => push('/sign-up')}
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignIn
